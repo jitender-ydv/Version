b0861b791aad7388df1e34fbf34db33ae554b330
205117019 <yjitu8058>
2018-03-25 04:23:38 +0530

    firstcommit

---
+++ app/src/main/java/com/nitt/a205117019/version/LoginActivity.java
@@ -0,0 +1,128 @@
+package com.nitt.a205117019.version;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    EditText _emailText = (EditText)findViewById((R.id.input_email));
+    EditText _passwordText = (EditText)findViewById(R.id.input_password);
+    Button _loginButton = (Button)findViewById((R.id.btn_login));
+    TextView _signupLink = (TextView)findViewById(R.id.link_signup);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+    }
+
+    public void login(){
+        Log.d(TAG,"Login");
+
+        if(!validate()){
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        /*final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                0);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                },3000);
+
+*/
+
+    }
+
+    //@Override
+
+   /* protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }*/
+
+    /*@Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }*/
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+
+    public boolean validate(){
+        boolean valid = true;
+
+        String user_email  = _emailText.getText().toString();
+        String user_password = _passwordText.getText().toString();
+
+        if(user_email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(user_email)
+                .matches()){
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        }else{
+            _emailText.setError(null);
+        }
+
+        if(user_password.isEmpty() || user_password.length()>4 || user_password.length()<12){
+            _passwordText.setError("enter a valid email address");
+            valid = false;
+        }else{
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+
+    public void onLoginFailed(){
+        Toast.makeText(getBaseContext(),"Login failed",Toast.LENGTH_LONG).show();
+        _loginButton.setEnabled(true);
+    }
+}
--- app/src/main/java/com/nitt/a205117019/version/MainActivity.java
+++ app/src/main/java/com/nitt/a205117019/version/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nitt.a205117019.version;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for(int i=0;i<10;i++)
-                System.out.println("foo");
+
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
     }
-}//new changes are commirted
+}
